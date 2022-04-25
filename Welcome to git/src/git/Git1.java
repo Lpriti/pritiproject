81a78ac30301b9c652c2b243d17f1e111a0ce18a
Lpriti <104313684+Lpriti@users.noreply.github.com>
2022-04-25 12:51:54 +0530

    welcome

---
+++ Welcome to git/src/git/Git1.java
@@ -0,0 +1,13 @@
+package git;
+
+public class Git1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int num1=10,num2=6,sum;
+		 sum=num1+num2;
+		 System.out.println("sum of the number: "+sum);
+
+	}
+
+}
